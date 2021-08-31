2740382cf99843333100242d0a69cbe8ed29e018
lsdany <luisdany@gmail.com>
2021-08-31 07:24:58 -0600

    Conversion de Linea a objeto de proceso

--- main.cpp
+++ main.cpp
@@ -1,46 +1,114 @@
+/*
+ * Grupo 2, Sistemas operativos
+ *
+ * Luisdany Israel Pernillo Gonzalez  7690-13-12063
+ *
+ *
+ *
+ */
+
+
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include "proceso.h"
+#include <vector>
+
+vector<string> getProcessByLine();
+
+vector<Proceso> armarProcesos(vector<string> vector1);
 
 using namespace std;
 
 int main() {
 
-    std::ifstream ifs("/home/luisdany/CLionProjects/test/test.txt");
-    std::string content((std::istreambuf_iterator<char>(ifs)),
-                        (std::istreambuf_iterator<char>()));
 
-    cout << content + "\n";
+    /*
+     * Paso1
+     * leer el archivo uno a uno
+     * y crear un vector con el total de lineas
+     */
 
-    std::string delimitador = "|";
+    vector<string> lineas = getProcessByLine();
 
-    size_t pos = 0;
-    std::string token;
-    while ((pos = content.find(delimitador)) != std::string::npos) {
-        token = content.substr(0, pos);
-        std::cout << token << std::endl;
-        content.erase(0, pos + delimitador.length());
-    }
-    std::cout << content << std::endl;
+    /*
+     * Paso 2
+     * Procesar cada linea para armar los objetos de los proceso
+     */
 
-    Proceso proc;
-    proc.procesoId = "1";
+    vector<Proceso> procesos = armarProcesos(lineas);
 
-    cout << proc.procesoId;
+    /*
+     * Paso 3
+     * crear la logica de los procesos
+     */
+
+
+    /**
+     * Paso 4
+     * Guardar bitacora, esto se puede ir haciendo durante el paso 3
+     * solo se debe tener lista una funcion que requiera un objeto o parametros
+     * que se van a escribir en el archivo
+     */
+
+
+    /**
+     * Paso 5
+     * pintar los procesos
+     */
 
 
     return 0;
 
 }
 
+vector<Proceso> armarProcesos(vector<string> vector1) {
+    size_t posicion = 0;
+    vector<Proceso> procesos;
 
-void parseString2(){
-//    string input("geeks\tfor\tgeeks");
-//    vector<string> result;
-//    boost::split(result, input, boost::is_any_of("\t"));
-//
-//    for (int i = 0; i < result.size(); i++)
-//        cout << result[i] << endl;
+    for (string cadena: vector1){
+        cout << "Cadena original: " +cadena << endl;
+        string token;
+        string delimitador = "|";
+
+        vector<string> v;
+
+
+        while ((posicion = cadena.find(delimitador)) != std::string::npos) {
+            token = cadena.substr(0, posicion);
+            cout << token << endl;
+            v.push_back(token);
+            cadena.erase(0, posicion + delimitador.length());
+        }
+
+        cout << v.size() << endl;
+
+        Proceso proceso = Proceso::parseProcess(v);
+        proceso.toString();
+
+        //TODO array to object Proceso by index
+        /*
+         * |0| -> procesoId
+         * |1| -> Tema (descripcion)
+         * |2| -> Prioridad
+         * |3| -> Paternidad
+         * |4| -> tiempo de ejecucion
+         *
+         */
+
+        procesos.push_back(proceso);
+    }
+
+    return procesos;
 }
+
+//METODO TEMPORAL, VA A SER REEMPLAZADO POR EL METODO DE LECTURA
+vector<string> getProcessByLine() {
+    vector<string> lineas {"4|Java|1|1|10|"};
+    return lineas;
+}
+
+
+
 
